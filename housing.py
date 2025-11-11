7865b05cb0f60a0b1f83bd502789dbfda3dae8f6
Dorit Yelshevich <dorityel@gmail.com>
2025-11-11 18:07:16 +0200

    Refactor data inspection section in housing.py by commenting out unused print statements and visualizations

--- housing.py
+++ housing.py
@@ -16,9 +16,23 @@ import seaborn as sns
 housing = pd.DataFrame(pd.read_csv("/Users/idan/Documents/Housing.csv"))
 
 # Data Inspection
-print (housing.head())
-print(housing.shape)
-print (housing.info())
-print (housing.describe())
 
-print(housing.isnull().sum()*100/housing.shape[0])
+print (housing.head())
+# print(housing.shape)
+# print (housing.info())
+# print (housing.describe())
+
+# print(housing.isnull().sum()*100/housing.shape[0])5
+# fig, axs = plt.subplots(2,3, figsize = (10,5))
+# plt1 = sns.boxplot(housing['price'], ax = axs[0,0])
+# plt2 = sns.boxplot(housing['area'], ax = axs[0,1])
+# plt3 = sns.boxplot(housing['bedrooms'], ax = axs[0,2])
+# plt1 = sns.boxplot(housing['bathrooms'], ax = axs[1,0])
+# plt2 = sns.boxplot(housing['stories'], ax = axs[1,1])
+# plt3 = sns.boxplot(housing['parking'], ax = axs[1,2])
+
+# plt.tight_layout()
+
+# plt.show()
+# sns.pairplot(housing)
+# plt.show()
